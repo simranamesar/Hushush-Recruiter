c80b07221908c066c26d526ca71b601cdc4ef111
Simran Amesar <48689580+simranamesar@users.noreply.github.com>
2026-02-16 11:00:11 +0100

    Refactor CODECHEF_LINK assignment for readability

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
 import streamlit as st
 
 EMAIL_CONFIG = st.secrets["EMAIL_CONFIG"]
+
+EMAIL_SENDER = EMAIL_CONFIG.get("sender")
+EMAIL_PASSWORD = EMAIL_CONFIG.get("password")
+SMTP_SERVER = EMAIL_CONFIG.get("smtp_server")
+SMTP_PORT = EMAIL_CONFIG.get("smtp_port")
+
 EMAIL_SUBJECT = "You have been Shortlisted!"
 DEADLINE = st.secrets.get("DEADLINE", "20 February, 2026 17:00")
-CODECHEF_LINK = st.secrets.get("CODECHEF_LINK", "https://www.codechef.com/practice/course/python/LPPYAS01/problems/LPYAS01")
-
+CODECHEF_LINK = st.secrets.get(
+    "CODECHEF_LINK",
+    "https://www.codechef.com/practice/course/python/LPPYAS01/problems/LPYAS01"
+)
